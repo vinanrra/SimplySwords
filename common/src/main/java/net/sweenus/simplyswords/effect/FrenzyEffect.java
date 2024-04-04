670eb089e2a4fd6482968db25223ca26a882f6c4
Sweenus <48658188+Sweenus@users.noreply.github.com>
2024-04-04 19:11:07 +1100

    Reworks & Balance - Wickpiercer & Ribboncleaver effects are now lost when swapping weapons, and can no longer be used for hot-swapping

--- common/src/main/java/net/sweenus/simplyswords/effect/FrenzyEffect.java
+++ common/src/main/java/net/sweenus/simplyswords/effect/FrenzyEffect.java
@@ -3,6 +3,8 @@ package net.sweenus.simplyswords.effect;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffectCategory;
 import net.minecraft.particle.ParticleTypes;
+import net.sweenus.simplyswords.item.custom.WickpiercerSwordItem;
+import net.sweenus.simplyswords.registry.EffectRegistry;
 
 public class FrenzyEffect extends OrbitingEffect {
     public FrenzyEffect(StatusEffectCategory statusEffectCategory, int color) {
@@ -12,6 +14,10 @@ public class FrenzyEffect extends OrbitingEffect {
 
     @Override
     public void applyUpdateEffect(LivingEntity livingEntity, int amplifier) {
+        if (!livingEntity.getWorld().isClient()) {
+            if (!(livingEntity.getMainHandStack().getItem() instanceof WickpiercerSwordItem) && !(livingEntity.getOffHandStack().getItem() instanceof WickpiercerSwordItem))
+                livingEntity.removeStatusEffect(EffectRegistry.FRENZY.get());
+        }
         super.applyUpdateEffect(livingEntity, amplifier);
     }
 
--- common/src/main/java/net/sweenus/simplyswords/effect/RibboncleaveEffect.java
+++ common/src/main/java/net/sweenus/simplyswords/effect/RibboncleaveEffect.java
@@ -3,6 +3,8 @@ package net.sweenus.simplyswords.effect;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffectCategory;
 import net.minecraft.particle.ParticleTypes;
+import net.sweenus.simplyswords.item.custom.RibboncleaverSwordItem;
+import net.sweenus.simplyswords.registry.EffectRegistry;
 
 public class RibboncleaveEffect extends OrbitingEffect {
     public RibboncleaveEffect(StatusEffectCategory statusEffectCategory, int color) {
@@ -12,6 +14,10 @@ public class RibboncleaveEffect extends OrbitingEffect {
 
     @Override
     public void applyUpdateEffect(LivingEntity livingEntity, int amplifier) {
+        if (!livingEntity.getWorld().isClient()) {
+            if (!(livingEntity.getMainHandStack().getItem() instanceof RibboncleaverSwordItem))
+                livingEntity.removeStatusEffect(EffectRegistry.RIBBONCLEAVE.get());
+        }
         super.applyUpdateEffect(livingEntity, amplifier);
     }
 
